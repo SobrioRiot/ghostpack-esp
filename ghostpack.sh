a7edfa890b9156698d0944aef5b18b81d004d690
Howard Roark <110576526+HowardlRoark@users.noreply.github.com>
2022-08-28 17:12:14 -0400

    ghostpack.sh

---
+++ ghostpack.sh
@@ -0,0 +1,73 @@
+## GHOSTPACK
+## Autoinstalación de un entorno avanzado dedicado al hacking ético 
+        ## Esta herramienta está hecha para ejecutarse después de una nueva instalación de Kali Linux.
+
+## Incluye
+
+- PTF
+- Recogida de información
+- Análisis de Vulnerabilidad
+- Análisis de aplicaciones web
+- Evaluación de la base de datos
+- Ataques a contraseñas
+- Ataques Wireless
+- Ingeniería inversa
+- Herramientas de explotación
+- Sniffing y Spoofing
+- Explotación posterior
+- Análisis forense
+- Anti-Forense
+- Herramientas de información
+- Herramientas de ingeniería social
+- Putty
+- Sysinternals de Windows
+- Rustscan
+- Dorking
+- Fuzzing
+- Recon
+- Ddos
+- Conchas
+- Gem
+- Docker
+- Metasploit-framework
+- Armitage
+- GO
+- Python3
+
+....¡y más!
+
+
+![GS](https://user-images.githubusercontent.com/64184513/176806645-6968abdb-3f38-447f-b20c-9426f00b7413.jpg)
+
+
+### Instalación:
+
+1. Actualizar
+
+        apt-get update && apt-get upgrade -y
+        
+2. Clonar
+
+        git clone https://github.com/NeverWonderLand/ghostpack
+
+3. Ejecuta
+
+        cd ghostpack
+    
+        chmod +x ghostpack.sh
+    
+        ./ghostpack.sh
+
+4. Relájate
+
+        Ve a dar un paseo (unas 3 horas)
+
+---------------------------------------
+* Sigue en el trabajo
+
+ ------------------
+| hecho por WONDR |
+| && USERWARE |
+| #GHOSTSEC |
+
+| ES Howard Roark|
